d93a66021a72e2d4e9abd25f9107b78cd69cd3d3
JustAddAcid <roman@nosoff.info>
2020-05-24 14:37:56 +0300

    fix og:image in posts

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
+// import { HOME_OG_IMAGE_URL } from '../lib/constants'
 
 export default function Meta() {
   return (
@@ -36,7 +36,7 @@ export default function Meta() {
         name="description"
         content={`Nosoff.info -- personal blog`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      {/* <meta property="og:image" content={HOME_OG_IMAGE_URL} /> */}
     </Head>
   )
 }
