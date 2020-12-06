f1042268affe39292894aa70abadf85ec2afd434
Roman A. Nosov <roman@nosoff.info>
2020-12-06 23:55:52 +0300

    tags builder

---
+++ pages/tags/[tag].js
@@ -0,0 +1,66 @@
+import Container from '../../components/container'
+import Layout from '../../components/layout'
+import { getAllTagsWithPosts } from '../../lib/api'
+import Head from 'next/head'
+import Container from '../components/container'
+import MoreStories from '../components/more-stories'
+import HeroPost from '../components/hero-post'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import Head from 'next/head'
+
+export default function Index({ allPosts, tag }) {
+    const heroPost = allPosts[0]
+    const morePosts = allPosts.slice(1)
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>Nosoff.info #{tag}</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            titleEnabled={heroPost.titleEnabled}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </Layout>
+        </>
+    )
+}
+
+export async function getStaticProps({ params }) {
+
+    const posts = params.posts;
+    posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    return {
+        props: { posts, tag: params.tag },
+    }
+}
+
+export async function getStaticPaths() {
+    const tagsMap = getAllTagsWithPosts()
+    const tags = Object.keys(tagsMap)
+
+    return {
+        paths: tags.map(tag => {
+            return {
+                params: {
+                    tag,
+                    posts: tagsMap[tag]
+                },
+            }
+        }),
+        fallback: false,
+    }
+}
