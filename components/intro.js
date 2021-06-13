8dea173ef0796d3c6f984970c1ad7926b7fc1d5c
Roman A. Nosov <roman@nosoff.info>
2021-06-13 13:08:20 +0400

    intro with link

--- components/intro.js
+++ components/intro.js
@@ -1,11 +1,13 @@
 import { CMS_NAME } from '../lib/constants'
 
-export default function Intro({tag}) {
+export default function Intro({ tag }) {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Nosoff.info { tag ? tag : '' }
-      </h1>
+      <a href="/">
+        <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          Nosoff.info {tag ? tag : ''}
+        </h1>
+      </a>
     </section>
   )
 }
